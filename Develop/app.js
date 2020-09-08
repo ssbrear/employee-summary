6bb481e1806b71281f928d811456d7a5f1182537
Sebastian Brear <ssbrear@ucdavis.edu>
2020-09-08 13:57:57 -0700

    Refactored to avoid callback hell

--- Develop/app.js
+++ Develop/app.js
@@ -12,83 +12,99 @@ const render = require("./lib/htmlRenderer");
 const { inherits } = require("util");
 
 async function init() {
-    inquirer
-        .prompt(
+    // Figures out how many times to loop the questions
+    const answer = await inquirer.prompt({
+        type: 'input',
+        message: 'How many team members are working on this project?',
+        name: 'numMembers'
+    })
+
+    console.log(`\nThere are ${answer.numMembers} member of this team. Please give information about each member.\n`);
+    let members = [];
+    
+    // Loops one time for each member
+    for (let i = 0; i < answer.numMembers; i++) {
+        const response = await inquirer.prompt([
             {
                 type: 'input',
-                message: 'How many team members are working on this project?',
-                name: 'numMembers'
+                message: 'Please insert your name',
+                name: 'name'
+            },
+            {
+                type: 'input',
+                message: 'Please insert your employee id',
+                name: 'id'
+            },
+            {
+                type: 'input',
+                message: 'Please insert your email',
+                name: 'email'
+            },
+            {
+                type: 'list',
+                message: 'Please indicate your role in this project',
+                name: 'role',
+                choices: ['Manager', 'Engineer', 'Intern']
             }
-        )
-        .then(async answer => {
-            const memberCount = answer.numMembers;
-            console.log(`\nThere are ${memberCount} member of this team. Please give information about each member.\n`);
-            for (let i = 0; i < memberCount; i++) {
-                await inquirer
-                    .prompt([
-                        {
-                            type: 'input',
-                            message: 'Please insert your name',
-                            name: 'name'
-                        },
-                        {
-                            type: 'input',
-                            message: 'Please insert your employee id',
-                            name: 'id'
-                        },
-                        {
-                            type: 'input',
-                            message: 'Please insert your email',
-                            name: 'email'
-                        },
-                        {
-                            type: 'list',
-                            message: 'Please indicate your role in this project',
-                            name: 'role',
-                            choices: ['Manager', 'Engineer', 'Intern']
-                        }
-                    ])
-                    .then(async response => {
-                        if (response.role === 'Manager') {
-                            await inquirer
-                                .prompt(
-                                    {
-                                        type: 'input',
-                                        message: 'What is your office number?',
-                                        name: 'officeNumber'
-                                    }
-                                )
-                                .then(result => {
-                                    //Create manager object
-                                })
-                        } else if (response.role === 'Engineer') {
-                            await inquirer
-                                .prompt(
-                                    {
-                                        type: 'input',
-                                        message: 'What is your Github username?',
-                                        name: 'github'
-                                    }
-                                )
-                                .then(result => {
-                                    //Create engineer object
-                                })
-                        } else if (response.role === 'Intern') {
-                            await inquirer
-                                .prompt(
-                                    {
-                                        type: 'input',
-                                        message: 'What is the name of your academic institution?',
-                                        name: 'school'
-                                    }
-                                )
-                                .then(result => {
-                                    //Create intern object
-                                })
-                        }
-                    })
-            }
-        })
+        ]);
+
+        // Checks for the role and asks additional questions
+        if (response.role === 'Manager') {
+            await inquirer
+                .prompt(
+                    {
+                        type: 'input',
+                        message: 'What is your office number?',
+                        name: 'officeNumber'
+                    }
+                )
+                .then(result => {
+                    console.log("\n");
+                    members.push(new Manager(response.name, response.id, response.email, result.officeNumber));
+                });
+        } else if (response.role === 'Engineer') {
+            await inquirer
+                .prompt(
+                    {
+                        type: 'input',
+                        message: 'What is your Github username?',
+                        name: 'github'
+                    }
+                )
+                .then(result => {
+                    console.log("\n");
+                    members.push(new Engineer(response.name, response.id, response.email, result.github));
+                });
+        } else if (response.role === 'Intern') {
+            await inquirer
+                .prompt(
+                    {
+                        type: 'input',
+                        message: 'What is the name of your academic institution?',
+                        name: 'school'
+                    }
+                )
+                .then(result => {
+                    console.log("\n");
+                    members.push(new Intern(response.name, response.id, response.email, result.school));
+                });
+        }
+    }
+    // Creates a txt file in JSON format
+    fs.writeFile('./output/members.txt', JSON.stringify(members, null, 4), (err) => {
+        if (err) {
+            throw err;
+        }
+        console.log("Text file of member info has been created in JSON format in the 'output' directory\n");
+        render(members);
+    });
+    // Creates the HTML file
+    fs.writeFile('./output/team.html', render(members), err => {
+        if (err) {
+            throw err;
+        }
+        console.log("HTML file of member info has been created in the 'output' directory\n");
+    })
 }
 
 init();
